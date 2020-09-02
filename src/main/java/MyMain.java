9f594f8f2c97615c5ab2ff6f8702c871bdb64c48
Nathan Lin <nathanlukelin@gmail.com>
2020-09-02 19:11:03 +0000

    First draft

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,7 +1,30 @@
-public class MyMain {
-    public static void main(String[] args) {
-        MyClass myClass = new MyClass("Hello World!");
+import java.util.Scanner;
 
-        System.out.println(myClass.getGreeting());
+public class MyMain {
+
+    // Calculates the median of the three inputs
+    public static int median(int a, int b, int c) {
+        return 0; // REPLACE WITH YOUR CODE
+    }
+
+    // Returns the input with the larger absolute value
+    public static int magnitude(int a, int b) {
+        return 0; // REPLACE WITH YOUR CODE
+    }
+
+    // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
+    // where "a" and "b" are the inputs to the method
+    public static double pythagoras(int a, int b) {
+        return 0.0; // REPLACE WITH YOUR CODE
+    }
+
+    public static void main(String[] args) {
+        // You may want to keep these lines of code to test that your methods work
+        System.out.println(median(1, 2, 3)); // should be 2
+        System.out.println(magnitude(-7, -1)); // should be -7
+        System.out.println(pythagoras(3, 4)); // should be 5.0
+
+        Scanner scan = new Scanner(System.in);
+        // YOUR CODE HERE
     }
 }
